36af6878369410c6b027f39da27fb78765034633
Maciej Sypien <maciejsypien@gmail.com>
2014-11-29 10:21:03 +0100

    improve repository installer

--- setup/run.py
+++ setup/run.py
@@ -7,68 +7,99 @@ import subprocess
 from colors import Colors
 
 # helper variables
-home_dir = os.path.expanduser('~')
-vim_dir = home_dir + "/.vim"
-vimrc = home_dir + "/.vimrc"
-tmux_conf = home_dir + "/.tmux.conf"
-
+home_dir                    = os.path.expanduser('~')
+__dotfiles_dir__            = home_dir + "/dot-files"
+__vim_dir_name__            = ".vim"
+__vim_dir__                 = home_dir + "/" + __vim_dir_name__
+__vimrc_name__              = ".vimrc"
+__vimrc__                   = home_dir + "/" + __vimrc_name__
+__tmux_conf_name__          = ".tmux.conf"
+__tmux_conf__               = home_dir + "/" + __tmux_conf_name__
+__gitconfig_name__          = ".gitconfig"
+__gitconfig__               = home_dir + "/" + __gitconfig_name__
+__gitconfig_local_name__    = ".gitconfig.local"
+__gitconfig_local__         = home_dir + "/" + __gitconfig_local_name__
 
 # Vim setup
 print(Colors.HEADER + "=== Vim setup ===" + Colors.ENDC)
-# updating .vimrc
-if os.path.isfile(vimrc):
-    print(".vimrc config file exist")
+# updating .__vimrc__
+if os.path.isfile(__vimrc__):
+    print(__vimrc_name__ + "config file exist")
 
-    if os.path.islink(vimrc):
+    print(__vimrc_name__+ " ", end="")
+    if os.path.islink(__vimrc__):
         # file is symlink
-        print(".vimrc is " + Colors.OKBLUE + "symlink." + Colors.ENDC)
+        print("is " + Colors.OKBLUE + "symlink." + Colors.ENDC)
     else:
-        print(".vimrc is simple file")
+        print("is simple file")
 
-    print("Making save copy of .vimrc file ... ", end="")
-    os.rename(vimrc, home_dir + "/.vimrc(" + time.strftime("%c") + ")")
+    print("Making save copy of " + __vimrc_name__ + " file ... ", end="")
+    os.rename(__vimrc__, __vimrc__ + "(" + time.strftime("%c") + ")")
     print(Colors.OKGREEN + "DONE" + Colors.ENDC)
 
 print("Creating symlink for .vimrc file ... ", end="")
-os.symlink(home_dir + "/dot-files/.vimrc", vimrc)
+os.symlink(__dotfiles_dir__ + "/" + __vimrc_name__, __vimrc__)
 print(Colors.OKGREEN + "DONE" + Colors.ENDC)
 
 
 # updating .vim directory
-if os.path.isdir(vim_dir):
-    print(".vim directory exist")
+if os.path.isdir(__vim_dir__):
+    print(__vim_dir_name__ + " directory exist")
 
-    if os.path.islink(vim_dir):
-        print(".vim is a symlink")
+    print(__vim_dir_name__ + " ", end="")
+    if os.path.islink(__vim_dir__):
+        print("is a symlink")
     else:
-        print(".vim is simple directory")
+        print("is simple directory")
 
-    print("Making save copy of .vim directory ... ", end="")
-    os.rename(vim_dir, home_dir + "/.vim(" + time.strftime("%c") + ")")
+    print("Making save copy of " + __vim_dir_name__ + " directory ... ", end="")
+    os.rename(__vim_dir__, __vim_dir__ + "(" + time.strftime("%c") + ")")
     print(Colors.OKGREEN + "DONE" + Colors.ENDC)
 
-print("Creating symlink for .vim directory ... ", end="")
-os.symlink(home_dir + "/dot-files/.vim", vim_dir)
+print("Creating symlink for " + __vim_dir_name__ + " directory ... ", end="")
+os.symlink(__dotfiles_dir__ + "/" + __vim_dir_name__, __vim_dir__)
 print(Colors.OKGREEN + "DONE" + Colors.ENDC)
 
 
 # tmux setup
 print(Colors.HEADER + "\n=== tmux setup ===" + Colors.ENDC)
-if os.path.isfile(tmux_conf):
-    print(".tmux.conf config file exsist.")
+if os.path.isfile(__tmux_conf__):
+    print(__tmux_conf_name__ + " config file exsist.")
 
-    if os.path.islink(tmux_conf):
+    print(__tmux_conf_name__ + " ", end="")
+    if os.path.islink(__tmux_conf__):
         # file is symlink
-        print(".tmux.conf is " + Colors.OKBLUE + "symlink." + Colors.ENDC)
+        print("is " + Colors.OKBLUE + "symlink." + Colors.ENDC)
     else:
-        print(".tmux.conf is simple file")
+        print("is simple file")
 
-    print("Making save copy of .tmux.conf file ... ", end="")
-    os.rename(tmux_conf, home_dir + "/.tmux.conf(" + time.strftime("%c") + ")")
+    print("Making save copy of " + __tmux_conf_name__ + " file ... ", end="")
+    os.rename(__tmux_conf__, __tmux_conf__ + "(" + time.strftime("%c") + ")")
     print(Colors.OKGREEN + "DONE" + Colors.ENDC)
 
-print("Creating symlink for .tmux.conf file ... ", end="")
-os.symlink(home_dir + "/dot-files/.tmux.conf", tmux_conf)
+print("Creating symlink for " + __tmux_conf_name__ + " file ... ", end="")
+os.symlink(__dotfiles_dir__ + "/" + __tmux_conf_name__, __tmux_conf__)
 print(Colors.OKGREEN + "DONE" + Colors.ENDC)
 
 
+# git setup
+print(Colors.HEADER + "\n=== Git setup ===" + Colors.ENDC)
+if os.path.isfile(__gitconfig__):
+    print(__gitconfig_name__ + " file exsist.")
+
+    print(__gitconfig_name__ + " ", end="")
+    if os.path.islink(__gitconfig__):
+        # file is symlink
+        print("is " + Colors.OKBLUE + "symlink." + Colors.ENDC)
+    else:
+        print("is simple file")
+
+    print("Making save copy of " + __gitconfig_name__ + " file ... ", end="")
+    os.rename(__gitconfig__, __gitconfig__ + "(" + time.strftime("%c") + ")")
+    print(Colors.OKGREEN + "DONE" + Colors.ENDC)
+
+print("Creating symlink for .gitconfig file ... ", end="")
+os.symlink(__dotfiles_dir__ + "/" + __gitconfig_name__, __gitconfig__)
+print(Colors.OKGREEN + "DONE" + Colors.ENDC)
+
+#
