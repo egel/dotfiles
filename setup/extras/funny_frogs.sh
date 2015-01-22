3d92f6d95b5f0cf56deb1ccea7e93d6a0f5ab390
Maciej Sypien <maciejsypien@gmail.com>
2015-01-22 18:15:49 +0100

    add setup script for terminal support of 256 colors

--- setup/extras/funny_frogs.sh
+++ setup/extras/funny_frogs.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 green='\e[0;32m' # '\e[1;32m' is too bright for white bg.
+red='\e[0;31m'
+yellow='\e[0;33m'
+purple='\e[0;35m'
 endColor='\e[0m'
 
-"====== Install: cowsay, fortune-mod ======"
+echo -e "${purple}====== Install: cowsay, fortune-mod ======${endColor}"
 sudo apt-get install -y cowsay fortune-mod
 echo '' >> ~/.bashrc && \
 echo '# Add funny-dummy text to new instance of teminal' >> ~/.bashrc && \
 echo 'fortune | cowsay -W80 -f bud-frogs' >> ~/.bashrc
-echo "${green}Done${endColor}"
+echo -e "${green}✔ Done${endColor}"
