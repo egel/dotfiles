a78b0446425c915345ce0c853e86d0211a469b16
Maciej Sypien <maciejsypien@gmail.com>
2015-01-24 03:09:54 +0100

    improve saving settings and change colors for bash

--- setup/extras/funny_frogs.sh
+++ setup/extras/funny_frogs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-green='\e[0;32m' # '\e[1;32m' is too bright for white bg.
+green='\e[1;32m' # '\e[1;32m' is too bright for white bg.
 red='\e[0;31m'
 yellow='\e[0;33m'
 purple='\e[0;35m'
