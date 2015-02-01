a061bbf2583a8b63381e6ead3269606352c85603
Maciej Sypień <maciej.sypien@gmail.com>
2015-02-01 15:21:31 +0100

    add simple script that clean installation of this repo

---
+++ setup/clean_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+green='\e[0;32m' # '\e[1;32m' is too bright for white bg.
+red='\e[0;31m'
+yellow='\e[0;33m'
+purple='\e[0;35m'
+endColor='\e[0m'
+
+echo -e "${yellow}====== Clean installation files of egel dot-files ======${endColor}"
+echo -e "${purple}Removing previous soft links to dot-files${endColor}"
+rm .vim\(* .vimrc\(* .gitconfig\(* .bash_aliases\(* .conkyrc\(* .tmux.conf\(*^
