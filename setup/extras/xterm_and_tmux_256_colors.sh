3312f582d151ff0f03dff6950b9024fe5c0dfba3
Maciej Sypien <maciejsypien@gmail.com>
2015-03-13 14:11:20 +0100

    fix loosing vim colorscheme in tmux mode

--- setup/extras/xterm_and_tmux_256_colors.sh
+++ setup/extras/xterm_and_tmux_256_colors.sh
@@ -9,6 +9,5 @@ endColor='\e[0m'
 echo -e "${purple}====== Adding config to ~/.bashrc ======${endColor}"
 echo "" >> ~/.bashrc && \
 echo "# Manually add 256colors support for xTerm and tmux" >> ~/.bashrc && \
-echo "export TERM=xterm-256color" >> ~/.bashrc && \
-echo "alias tmux=\"tmux -2\"" >> ~/.bashrc
+echo "alias tmux=\"TERM=screen-256color-bce tmux\"" >> ~/.bashrc
 echo -e "${green}✔ Done${endColor}"
