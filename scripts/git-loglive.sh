5e42b57c176006687566d6d7a23e4fc9cd1dce9a
Maciej Sypien <maciejsypien@gmail.com>
2015-04-20 16:35:57 +0200

    improve git-loglive.sh

--- scripts/git-loglive.sh
+++ scripts/git-loglive.sh
@@ -2,13 +2,14 @@
 while true;
 do
     clear
+    echo "Git livelog: $(pwd)"
     git log \
     --graph \
     --all \
     --abbrev-commit \
     --date=relative \
-    -30 \
+    -20 \
     --format=format:'%C(bold red)%h%C(reset) %C(bold magenta)(%ar)%C(reset) %C(white)%s%C(reset) %C(bold blue) %an%C(reset)%C(bold yellow)%d%C(reset)' |
     cat -
-    sleep 5
+    sleep 3
 done
