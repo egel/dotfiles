997934de1c8c3b9cbd30bab907fb3e3a79ce299c
Maciej Sypien <maciejsypien@gmail.com>
2024-03-27 23:20:48 +0100

    feat: add core LSPs and linters

--- configuration/.config/nvim/lua/mason-config.lua
+++ configuration/.config/nvim/lua/mason-config.lua
@@ -1,49 +1,49 @@
-require("mason").setup(
-    {
-        ui = {
-            icons = {
-                package_installed = "✓",
-                package_pending = "➜",
-                package_uninstalled = "✗"
-            }
-        }
-    }
-)
+require("mason").setup({
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗",
+    },
+  },
+})
 
-require("mason-lspconfig").setup(
-    {
-        -- A list of servers to automatically install if they're not already
-        -- installed. Example: { "rust_analyzer@nightly", "lua_ls" }
-        --
-        -- This setting has no relation with the `automatic_installation` setting.
-        ---@type string[]
-        ensure_installed = {
-            "angularls",
-            "tsserver",
-            "lua_ls",
-            "helm_ls",
-            "bashls",
-            "docker_compose_language_service",
-            "dockerls",
-            --'shellcheck',
-            "sqlls",
-            "stylelint_lsp",
-            "yamlls", -- yaml language-server
-            "pyright",
-            "mdx_analyzer",
-            "cssmodules_ls",
-            -- "luacheck",
-            -- "luaformatter",
-            -- "nginx-language-server",
-            -- "java_language_server"
+require("mason-lspconfig").setup({
+  -- A list of servers to automatically install if they're not already
+  -- installed. Example: { "rust_analyzer@nightly", "lua_ls" }
+  --
+  -- This setting has no relation with the `automatic_installation` setting.
+  ---@type string[]
+  ensure_installed = {
+    -- LSP
+    "angularls",
+    "bashls",
+    "cssls",
+    "docker_compose_language_service",
+    "dockerls",
+    "eslint",
+    "helm_ls",
+    "lua_ls",
+    "pyright",
+    "sqlls",
+    "stylelint_lsp",
+    "tsserver",
+    "yamlls", -- yaml language-server
+    "zls",
+    -- Additionally install npm package vscode-langservers-extracted
+    --
+    -- @see https://github.com/hrsh7th/vscode-langservers-extracted
 
-            -- Additionally install npm package vscode-langservers-extracted
-            --
-            -- @see https://github.com/hrsh7th/vscode-langservers-extracted
-            "cssls",
-            "eslint",
-            "html",
-        },
-        automatic_installation = false
-    }
-)
+    -- Linter
+    "luacheck",
+    "markdownlint",
+    "shellcheck",
+    "yamllint",
+
+    -- Formatter
+    "beautysh",
+    "black",
+    "stylua",
+  },
+  automatic_installation = false,
+})
