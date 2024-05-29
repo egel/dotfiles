99ae99111d1801955ab864acd119acb50bba72bc
Maciej Sypien <maciejsypien@gmail.com>
2024-05-29 16:31:39 +0200

    fix(nvim): supress errors when lint

--- configuration/.config/nvim/lua/plugins/nvim-lint-config.lua
+++ configuration/.config/nvim/lua/plugins/nvim-lint-config.lua
@@ -17,11 +17,17 @@ return {
       -- kotlin = { "ktlint" },
       terraform = { "tflint" },
       ruby = { "standardrb" },
-      markdown = { "markdownlint" }, --
+      markdown = { "markdownlint", "cspell" }, --
       lua = { "luacheck" }, -- from :Mason
       yaml = { "yamllint" },
       css = { "stylelint" },
       scss = { "stylelint" },
+
+      -- Use the "*" filetype to run formatters on all filetypes.
+      ["*"] = {},
+      -- Use the "_" filetype to run formatters on filetypes that don't
+      -- have other formatters configured.
+      ["_"] = {},
     },
   },
   config = function()
@@ -29,15 +35,19 @@ return {
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
     -- Lint file content when: Enter Buffer, Save the buffer
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "TextChanged", "InsertLeave" }, {
       group = lint_augroup,
       callback = function()
-        lint.try_lint()
+        -- local ok, err = pcall(lint.try_lint, nil, {})
+        -- if not ok then
+        -- vim.notify(err, vim.log.levels.WARN)
+        -- end
+        lint.try_lint(nil, { ignore_errors = true })
       end,
     })
 
     vim.keymap.set("n", "<leader>ll", function()
-      lint.try_lint()
+      lint.try_lint(nil, { ignore_errors = true })
       vim.print("Tried linting")
     end, { desc = "Trigger linting for current file" })
   end,
