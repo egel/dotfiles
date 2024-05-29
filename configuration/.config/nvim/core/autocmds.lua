724c675a2d707799db640c459f8f1f602f139396
Maciej Sypien <maciejsypien@gmail.com>
2024-05-29 16:55:47 +0200

    feat(nvim): add foldmethod for JSON file types

--- configuration/.config/nvim/core/autocmds.lua
+++ configuration/.config/nvim/core/autocmds.lua
@@ -8,41 +8,42 @@ local api = vim.api
 
 -- Git:
 -- enable spell check when entering gitcommit file
-api.nvim_create_autocmd(
-    'FileType',
-    {
-        pattern = { 'gitcommit' },
-        command = 'setlocal spell',
-    }
-)
+api.nvim_create_autocmd("FileType", {
+  pattern = { "gitcommit" },
+  command = "setlocal spell",
+})
 
 -- Folding
 -- Set folding for vim files
-api.nvim_create_autocmd(
-    'FileType',
-    {
-        pattern = { 'vim' },
-        command = 'setlocal foldmethod=marker foldmarker={{{,}}}',
-    }
-)
+api.nvim_create_autocmd("FileType", {
+  pattern = { "vim" },
+  command = "setlocal foldmethod=marker foldmarker={{{,}}}",
+})
 
 -- Set folding for JavaScript/TypeScript files
-api.nvim_create_autocmd(
-    'FileType',
-    {
-        pattern = { 'javascript', 'typescript' },
-        command = 'setlocal foldmethod=marker foldmarker={,}',
-    }
-)
+api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "typescript" },
+  command = "setlocal foldmethod=marker foldmarker={,}",
+})
 
+-- Set folding for JSON files
+api.nvim_create_autocmd("FileType", {
+  pattern = { "json" },
+  command = "setlocal foldmethod=marker foldmarker={,}",
+})
+
+-- Set folding for SQL files
+api.nvim_create_autocmd("FileType", {
+  pattern = { "sql" },
+  command = "setlocal commentstring='-- %s'",
+})
 -- Run gofmt on save
 --
 local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
   callback = function()
-   require('go.format').gofmt()
+    require("go.format").gofmt()
   end,
   group = format_sync_grp,
 })
-
