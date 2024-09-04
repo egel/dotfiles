d6697bb64ba272cfb054e558d59d47e527eccb97
Maciej Sypien <maciejsypien@gmail.com>
2024-09-04 15:48:19 +0200

    fix(nvim): add missing web-devicons for lualine plugin

--- configuration/.config/nvim/lua/plugins/lualine-nvim-config.lua
+++ configuration/.config/nvim/lua/plugins/lualine-nvim-config.lua
@@ -1,4 +1,7 @@
 return {
   "nvim-lualine/lualine.nvim", -- Add nice bottom line in editor modes
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
   lazy = false, -- load during startup
 }
