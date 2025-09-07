525cfc42c679085260f1543cfcb10d5cccc9018c
Maciej Sypien <maciejsypien@gmail.com>
2025-09-07 11:15:40 +0200

    feat(nvim): add showing working/idle ollama status

--- configuration/.config/nvim/lua/plugins/lualine-nvim-config.lua
+++ configuration/.config/nvim/lua/plugins/lualine-nvim-config.lua
@@ -4,4 +4,23 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   lazy = false, -- load during startup
+
+  -- Enhance lualine with information about ollama status
+  optional = true,
+  opts = function(_, opts)
+    table.insert(opts.sections.lualine_x, {
+      function()
+        local status = require("ollama").status()
+
+        if status == "IDLE" then
+          return "󱙺" -- nf-md-robot-outline
+        elseif status == "WORKING" then
+          return "󰚩" -- nf-md-robot
+        end
+      end,
+      cond = function()
+        return package.loaded["ollama"] and require("ollama").status() ~= nil
+      end,
+    })
+  end,
 }
